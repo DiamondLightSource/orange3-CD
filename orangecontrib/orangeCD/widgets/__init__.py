0e6272a74774c64d55491c7c636d575f487542db
Chris Brasnett <christopher.brasnett@diamond.ac.uk>
2026-08-19 17:29:45 +0100

    reorganise to enable facilitate multiple CD widget categories

--- orangecontrib/orangeCD/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-NAME = "Orange CD"
-
-DESCRIPTION = "Tools for circular dichroism data analysis."
-
-ICON = "icons/Titration.svg"
-
-BACKGROUND = "#C7D9F1"
-
-PRIORITY = 100
---
+++ orangecontrib/orangeCD/widgets/titration/__init__.py
@@ -0,0 +1,11 @@
+NAME = "CD Titration"
+
+ID = "orangecontrib.orangeCD.widgets.titration"
+
+DESCRIPTION = "Tools for CD titration data analysis."
+
+ICON = "icons/Titration.svg"
+
+BACKGROUND = "#C7D9F1"
+
+PRIORITY = 100
