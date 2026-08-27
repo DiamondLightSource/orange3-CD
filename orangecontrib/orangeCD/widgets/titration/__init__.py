e10bcda95c03de0b93bd48bc90cb4c2e97cc6d5d
Chris Brasnett <christopher.brasnett@diamond.ac.uk>
2026-08-27 16:30:23 +0100

    add unit registry to module

--- orangecontrib/orangeCD/widgets/titration/__init__.py
+++ orangecontrib/orangeCD/widgets/titration/__init__.py
@@ -9,3 +9,9 @@ ICON = "icons/Titration.svg"
 BACKGROUND = "#C7D9F1"
 
 PRIORITY = 100
+
+# Use pint to handle units across the pipeline
+from pint import UnitRegistry, set_application_registry
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
+set_application_registry(ureg)
